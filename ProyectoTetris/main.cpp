16d39718f30a262368b16dd15fd097faeb912416
Das <143858513+DasVargas@users.noreply.github.com>
2026-08-29 10:19:39 -0600

    feat: se agrego menu de pruebas
    Se agregó un menú de pruebas para facilitar la verificación del tablero, piezas y cola de piezas, y permitir agregar fácilmente las pruebas de las siguientes implementaciones del proyecto

--- ProyectoTetris/main.cpp
+++ ProyectoTetris/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+
+#include "Tablero.h"
+#include "Pieza.h"
 #include "ColaPiezas.h"
 
 using namespace std;
@@ -8,46 +11,116 @@ using namespace std;
 int main() {
 	
 	srand(time(NULL));
+	int opcion;
 	
-	ColaPiezas cola;
+	do {
+		cout << "========== TETRIS ==========" << endl;
+		cout << "1. Iniciar juego" << endl;
+		cout << "2. Pruebas" << endl;
+		cout << "0. Salir" << endl;
+		cout << "============================" << endl;
+		cout << "Opcion: ";
+		cin >> opcion;
+		
+		switch (opcion) {
+			
+		case 1: {
+			system("cls");
+			cout << "======= TETRIS =======" << endl;
+			cout << "Juego pendiente..." << endl;
+			cout << endl;
+			system("pause");
+			break;
+		}
+		
+		case 2: {
+			int prueba;
+			do {
+				system("cls");
+				cout << "======= PRUEBAS =======" << endl;
+				cout << "1. Tablero" << endl;
+				cout << "2. Piezas" << endl;
+				cout << "3. Cola de piezas" << endl;
+				cout << "0. Volver" << endl;
+				cout << "========================" << endl;
+				cout << "Opcion: ";
+				cin >> prueba;
 	
-	cola.generarBolsa();
-	
-	cout << "COLA INICIAL:" << endl;
-	cola.mostrar();
-	
-	cout << endl;
-	
-	cout << "Sacamos 5 piezas:" << endl;
-	
-	for (int i = 0; i < 5; i++) {
-		cout << cola.sacar() << " ";
-	}
-	
-	cout << endl << endl;
-	
-	cout << "COLA RESTANTE:" << endl;
-	cola.mostrar();
-	
-	cout << endl;
-	
-	cout << "Cantidad restante: " << cola.contar() << endl;
-	
-	cola.mantenerCola();
-	
-	cout << endl;
-	
-	cout << "COLA DESPUES DE MANTENER:" << endl;
-	cola.mostrar();
-	
-	cout << endl;
-	
-	cout << "Cantidad ahora: " << cola.contar() << endl;
-	
-	cout << endl;
-	
-	cout << "PROXIMAS 3:" << endl;
-	cola.mostrarProximas3();
+				switch (prueba) {
+					
+				case 1: {
+					system("cls");
+					Tablero tablero;
+					cout << "======= TABLERO =======" << endl;
+					tablero.mostrarTablero();
+					cout << endl;
+					system("pause");
+					break;
+				}
+				
+				case 2: {
+					system("cls");
+					char tipo;
+					cout << "======= PIEZAS =======" << endl;
+					cout << "Pieza (I, O, T, S, Z, J, L): ";
+					cin >> tipo;
+					Pieza pieza(tipo);
+					cout << endl;
+					pieza.mostrarPieza();
+					cout << endl;
+					system("pause");
+					break;
+				}
+				
+				case 3: {
+					system("cls");
+					ColaPiezas cola;
+					cola.generarBolsa();
+					cout << "======= COLA DE PIEZAS =======" << endl;
+					cout << "Cola completa:" << endl;
+					cola.mostrar();
+					cout << endl;
+					cout << "Proximas 3:" << endl;
+					cola.mostrarProximas3();
+					cout << endl;
+					system("pause");
+					break;
+				}
+				
+				case 0:
+					break;
+				default: {
+					cout << "Opcion incorrecta." << endl;
+					system("pause");
+					
+					break;
+				}
+				}
+				
+			} while (prueba != 0);
+			
+			break;
+		}
+		
+		case 0:
+			
+			system("cls");
+			cout << "Saliendo..." << endl;
+			
+			break;
+			
+		default: {
+			
+			cout << endl;
+			cout << "Opcion incorrecta." << endl;
+			
+			system("pause");
+			
+			break;
+		}
+		}
+		
+	} while (opcion != 0);
 	
 	return 0;
 }
